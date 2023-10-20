317c5afbc0f901221126740ff1d677de9b80feea
MathiasSchjoedt-Bavngaard <81029612+MathiasSchjoedt-Bavngaard@users.noreply.github.com>
2023-10-20 14:29:24 +0200

    Made Hieroglyphs scale down on Scene Switch

--- TuuksTomb/Assets/HieroglyphPositions.cs
+++ TuuksTomb/Assets/HieroglyphPositions.cs
@@ -7,20 +7,33 @@ public class HieroglyphPositions : MonoBehaviour
 {
     public GameObject[] hieroglyphs;
     public bool sideView = true;
-    public float zPosition = 0;
-    
+    public float zPosition = -1000;
+    private const float ColorScale = 0.1f;
+    public float xScale = 0.05f;
+    public float yScale = 0.05f;
+
     // Start is called before the first frame update
     private void SaveHieroglyphPositions()
     {
-        
         foreach (var hieroglyph in hieroglyphs)
         {
+            if (PlayerPrefs.HasKey(hieroglyph.name + "z") == false)
+                PlayerPrefs.SetFloat(hieroglyph.name + "z", hieroglyph.transform.position.y);
+
+            if (sideView)
+            {
+                var heiroglyphZ = PlayerPrefs.GetFloat(hieroglyph.name + "z");
+
+                //if hieroglyph is not active or is further away than zPosition dont save it because it is not movable
+                if (hieroglyph.activeSelf == false || (sideView && !HieroglyphMovable(heiroglyphZ))) continue;
+            }
+
             var hieroglyphPosition = hieroglyph.transform.position;
             var hieroglyphX = hieroglyphPosition.x;
             var hieroglyphY = hieroglyphPosition.y;
-            
+
             PlayerPrefs.SetFloat(hieroglyph.name + "x", hieroglyphX);
-            if(sideView)
+            if (sideView)
             {
                 PlayerPrefs.SetFloat(hieroglyph.name + "y", hieroglyphY);
             }
@@ -28,10 +41,9 @@ public class HieroglyphPositions : MonoBehaviour
             {
                 PlayerPrefs.SetFloat(hieroglyph.name + "z", hieroglyphY);
             }
-           
         }
     }
-    
+
     // Making hieroglyphs visible and setting their positions
     private void SetHeirpglyphPositions()
     {
@@ -39,31 +51,51 @@ public class HieroglyphPositions : MonoBehaviour
         {
             foreach (var hieroglyph in hieroglyphs)
             {
-                var heirglyphZ = PlayerPrefs.GetFloat(hieroglyph.name + "z");
-                
-                //if heirglyphZ is in between 1 of zPosition set hieroglyph position to x and y else disable hieroglyph
-                if (zPosition  >= heirglyphZ -0.5 && zPosition < heirglyphZ + 0.5)
+                var heiroglyphZ = PlayerPrefs.GetFloat(hieroglyph.name + "z");
+
+
+                if (!HieroglyphVisible(heiroglyphZ))
                 {
+                    hieroglyph.SetActive(false);
+                    continue;
+                }
+
+               
+
+                //if hieroglyph dont have positions jet, set y position to current y position
+                if (!PlayerPrefs.HasKey(hieroglyph.name + "y"))
+                {
+                    var hieroglyphPosition = hieroglyph.transform.position;
+                    var hieroglyphYfirst = hieroglyphPosition.y;
+                    PlayerPrefs.SetFloat(hieroglyph.name + "y", hieroglyphYfirst);
+                }
+
+                hieroglyph.SetActive(true);
+                var hieroglyphX = PlayerPrefs.GetFloat(hieroglyph.name + "x");
+                var hieroglyphY = PlayerPrefs.GetFloat(hieroglyph.name + "y");
+                hieroglyph.transform.position = new Vector2(hieroglyphX, hieroglyphY);
+                
+                if (HieroglyphMovable(heiroglyphZ))
+                { 
                     PlayerPrefs.SetFloat(hieroglyph.name + "z", zPosition);
-                    
-                    //if hieroglyph dont have positions set them
-                    if (!PlayerPrefs.HasKey(hieroglyph.name + "y"))
-                    {
-                        var hieroglyphPosition = hieroglyph.transform.position;
-                        var hieroglyphYfirst = hieroglyphPosition.y;
-                        PlayerPrefs.SetFloat(hieroglyph.name + "y", hieroglyphYfirst);
-                    }
-                    
-                    
-                    
-                    hieroglyph.SetActive(true);
-                    var hieroglyphX = PlayerPrefs.GetFloat(hieroglyph.name + "x");
-                    var hieroglyphY = PlayerPrefs.GetFloat(hieroglyph.name + "y");
-                    hieroglyph.transform.position = new Vector2(hieroglyphX, hieroglyphY);
                 }
                 else
                 {
-                    hieroglyph.SetActive(false);
+                    // make hieroglyph 5% smaller for each unit it is further away than zPosition
+                    ScaleDownHieroglyph(hieroglyph, heiroglyphZ - zPosition);
+
+                    //make hieroglyph 10% darker for each unit it is further away than zPosition
+                    hieroglyph.GetComponent<SpriteRenderer>().color =
+                        ScaledownColor(hieroglyph.GetComponent<SpriteRenderer>().color, heiroglyphZ - zPosition);
+
+                    //make hieroglyph not falling
+                    hieroglyph.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+                    //remove colliders
+                    if (hieroglyph.GetComponent<BoxCollider2D>() != null)
+                    {
+                        hieroglyph.GetComponent<BoxCollider2D>().enabled = false;
+                    }
                 }
             }
         }
@@ -72,15 +104,13 @@ public class HieroglyphPositions : MonoBehaviour
         {
             foreach (var hieroglyph in hieroglyphs)
             {
-                
                 //if not player prefs set them
                 if (!PlayerPrefs.HasKey(hieroglyph.name + "x"))
                     PlayerPrefs.SetFloat(hieroglyph.name + "x", hieroglyph.transform.position.x);
                 if (!PlayerPrefs.HasKey(hieroglyph.name + "z"))
                     PlayerPrefs.SetFloat(hieroglyph.name + "z", hieroglyph.transform.position.y);
-                
-                     
-                      
+
+
                 hieroglyph.SetActive(true);
                 var hieroglyphX = PlayerPrefs.GetFloat(hieroglyph.name + "x");
                 var hieroglyphZ = PlayerPrefs.GetFloat(hieroglyph.name + "z");
@@ -94,6 +124,7 @@ public class HieroglyphPositions : MonoBehaviour
         SetHeirpglyphPositions();
     }
 
+
     private void Update()
     {
         //if z key pressed Save hieroglyph positions
@@ -101,13 +132,61 @@ public class HieroglyphPositions : MonoBehaviour
         {
             SaveHieroglyphPositions();
         }
-        
-        
+
+        //TODO: remove input as only for testing
         //if x key pressed Set hieroglyph positions
-        if(Input.GetKeyDown(KeyCode.B))
-           SetHeirpglyphPositions();
-        
-        if(Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.B))
+            SetHeirpglyphPositions();
+
+        if (Input.GetKeyDown(KeyCode.R))
             PlayerPrefs.DeleteAll();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+        SaveHieroglyphPositions();
+    }
+
+    private static Color ScaledownColor(Color color, float sideviewDif)
+    {
+        var procents = (ColorScale * sideviewDif);
+
+        //set color to colorScale% darker for each Difference in sideviewNumber
+        var redtoremove = color.r * procents;
+        color.r = color.r - redtoremove;
+        var greentoremove = color.g * procents;
+        color.g = color.g - greentoremove;
+        var bluetoremove = color.b * procents;
+        color.b = color.b - bluetoremove;
+        return color;
+    }
+
+    private void ScaleDownHieroglyph(GameObject hieroglyph, float sideviewDif)
+    {
+        //scale down hieroglyph for each unit it is further away than zPosition
+        var scale = hieroglyph.transform.localScale;
+        scale.x = scale.x - (xScale * sideviewDif);
+        scale.y = scale.y - (yScale * sideviewDif);
+        hieroglyph.transform.localScale = scale;
+                
+        //Move hieroglyph further away from camera
+        var position = hieroglyph.transform.position;
+        position.x = position.x * scale.x;
+        position.y = position.y * scale.y;
+        hieroglyph.transform.position = position;
+        
+        
+    }
+
+    //functions to tjeck hieroglyph active layer
+    private bool HieroglyphVisible(float hieroglyphZ)
+    {
+        return zPosition - 0.5 <= hieroglyphZ;
+    }
+
+    private bool HieroglyphMovable(float hieroglyphZ)
+    {
+        return zPosition + 0.5 > hieroglyphZ;
     }
 }
--- TuuksTomb/Assets/SideviewFader.cs
+++ TuuksTomb/Assets/SideviewFader.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
 
 public class SideviewFader : MonoBehaviour
 {
     public float xScale = 0.05f;
     public float yScale = 0.05f;
-    private float colorScale = 0.1f;
+    private const float ColorScale = 0.1f;
     public float ownSideviewNumber;
     public float sceneSideviewNumber;
 
     public bool seetrhough = false;
-    public GameObject[] ObjectsToTransform;
+    public GameObject[] objectsToTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -21,58 +22,60 @@ public class SideviewFader : MonoBehaviour
         var sideviewDif = sceneSideviewNumber - ownSideviewNumber;
         if (sideviewDif != 0)
         {
-            foreach (var gameObject in ObjectsToTransform)
+            foreach (var objectToTransform in objectsToTransform)
             {
-                var color = gameObject.GetComponent<Tilemap>().color;
+                var color = objectToTransform.GetComponent<Tilemap>().color;
                 if (seetrhough)
                 {
-                    color.a = color.a - (colorScale * sideviewDif);
+                    color.a = color.a - (ColorScale * sideviewDif);
                 }
-                gameObject.GetComponent<Tilemap>().color = ScaledownColor(color, sideviewDif);
+                objectToTransform.GetComponent<Tilemap>().color = ScaledownColor(color, sideviewDif);
 
                 //set order in layer to 1 less pr difference in sideviewNumber
-                var order = gameObject.GetComponent<TilemapRenderer>().sortingOrder;
+                var order = objectToTransform.GetComponent<TilemapRenderer>().sortingOrder;
                 order = (int)(order - sideviewDif);
-                gameObject.GetComponent<TilemapRenderer>().sortingOrder = order;
+                objectToTransform.GetComponent<TilemapRenderer>().sortingOrder = order;
             }
 
 
             //set the scale of this object to 5% less pr difference in sideviewNumber
-            var scale = transform.localScale;
+            var transform1 = transform;
+            var scale = transform1.localScale;
             scale.x = scale.x - (xScale * sideviewDif);
             scale.y = scale.y - (yScale * sideviewDif);
-            transform.localScale = scale;
+            transform1.localScale = scale;
 
 
             //remove colliders for objects in other sideviews
-            foreach (var gameObject in ObjectsToTransform)
+            foreach (var o in objectsToTransform)
             {
-                if (gameObject.GetComponent<BoxCollider2D>() != null)
+                if (o.GetComponent<BoxCollider2D>() != null)
                 {
-                    gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                    o.GetComponent<BoxCollider2D>().enabled = false;
                 }
 
-                if (gameObject.GetComponent<CircleCollider2D>() != null)
+                if (o.GetComponent<CircleCollider2D>() != null)
                 {
-                    gameObject.GetComponent<CircleCollider2D>().enabled = false;
+                    o.GetComponent<CircleCollider2D>().enabled = false;
                 }
 
-                if (gameObject.GetComponent<PolygonCollider2D>() != null)
+                if (o.GetComponent<PolygonCollider2D>() != null)
                 {
-                    gameObject.GetComponent<PolygonCollider2D>().enabled = false;
+                    o.GetComponent<PolygonCollider2D>().enabled = false;
                 }
 
-                if (gameObject.GetComponent<TilemapCollider2D>() != null)
+                if (o.GetComponent<TilemapCollider2D>() != null)
                 {
-                    gameObject.GetComponent<TilemapCollider2D>().enabled = false;
+                    o.GetComponent<TilemapCollider2D>().enabled = false;
+                    
                 }
             }
         }
     }
     
-    private Color ScaledownColor(Color color, float sideviewDif)
+    private static Color ScaledownColor(Color color, float sideviewDif)
     {
-        var procents = (colorScale * sideviewDif);
+        var procents = (ColorScale * sideviewDif);
         
         //set color to colorScale% darker for each Difference in sideviewNumber
         var redtoremove = color.r * procents;
